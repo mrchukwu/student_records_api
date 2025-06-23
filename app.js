bc7915aa3f98f20538559a611134edb50905caf9
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-06-23 10:55:34 +0100

    student signup

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const Student =  require("./model/student");
+
 
 const app = express();
+app.use(express.json());
 
 app.use("/test", (req, res) => {
     res.send("<h1>Hello World</h1>");
 });
 
-connectDB().then(() => {
-  console.log("Database conection established...");
-  app.listen(3001, () => {
-    console.log("Server running on port 3001")
-  })
+app.post("/signup", async(req, res) => {
+  try{
+
+    console.log(req.body)
+    const {firstname, lastname, email, password, age} = req.body;
+    const student = new Student({
+      firstname, lastname, email, password, age
+    })
+
+    res.status(200).json({
+      status: "success",
+      message: "Student created",
+      student
+    })
+  }catch(err){
+    res.status(401).json({
+      status: "failed",
+      message: "erorr: " + err.message
+    })
+  }
 })
-.catch((err) => {
-    console.error("Database cannot be connected!!")
-});
+
+
+app.listen(3001, () => {
+  console.log("server running on port 3001")
+})
