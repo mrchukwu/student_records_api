6e5f2b8dc06ad7cdc99159fabcd6c0d6cd1d891a
Kennedy Chukwu <mrchukwusinbox@gmail.com>
2025-06-24 16:31:07 +0100

    update: minor code refactor config/database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-// const MongoBD_URI = "mongodb+srv://mrchukwusinbox:l1BPRO7z7Vzf8YX8@student-records.hf3ptve.mongodb.net/students";
 const MongoBD_URI = process.env.MongoBD_URI;
 
 const connectDB = async() => {
